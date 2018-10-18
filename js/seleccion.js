77bb416ad1ed3742dac3cefc1585f1c5c446395c
Maria  Reboyoso <lourdes@mujeresunidas.net>
2018-10-18 11:53:44 -0700

    cambios en js para click de los checkboxes

--- js/seleccion.js
+++ js/seleccion.js
@@ -19,5 +19,22 @@ object.addEventListener("click", );
 
 // onclick = function();
 
+function mySelection() {
+	document.getElementById("trabajos").click();
+	document.getElementById("etapas").click();
+	document.getElementById("educacion").click();
+	document.getElementById("domestica").click();
+	document.getElementById("AS").click();
+	document.getElementById("autoestima").click();
+	document.getElementById("salud").click();
+	document.getElementById("sana").click();
+	document.getElementById("CPR").click();
+	document.getElementById("universidad").click();
+	document.getElementById("inmigracion").click();
+	document.getElementById("hogar").click();
+	document.getElementById("recaudacion").click();
+
+}
+
 /*Despues de las 3 opciones elegidas, disabilitar los checkboxes que sobraron. 
 After 3 selected, make unselected checkboxes gray*/
