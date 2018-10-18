ce471a564e723abc14872413f81d674bbe23a9e2
Maria  Reboyoso <lourdes@mujeresunidas.net>
2018-10-17 17:57:28 -0700

    nuevo javascript de seleccion de checkboxes

---
+++ js/seleccion.js
@@ -0,0 +1,23 @@
+
+/*Crear un array para dar una lista de las 3 opciones seleccionadas. 
+Create array to give a list of the 3 cheboxes selected*/
+
+let clickedAnswers = [];
+let checkboxes = document.querySelectorAll('input[type=checkbox]');
+let checkboxes1 = document.querySelectorAll('preparacion>input[type=checkbox]');
+let checkboxes2 = document.querySelectorAll('apoyo>input[type=checkbox]');
+let checkboxes3 = document.querySelectorAll('raices>input[type=checkbox]');
+let checkboxes4 = document.querySelectorAll('fondos>input[type=checkbox]');
+/*Crear un event handler para darle clicks a los checkboxes, 
+Create an event handler for clicking checkbox groups*/
+
+object.onclick = function() {
+
+};
+
+object.addEventListener("click", );
+
+// onclick = function();
+
+/*Despues de las 3 opciones elegidas, disabilitar los checkboxes que sobraron. 
+After 3 selected, make unselected checkboxes gray*/
