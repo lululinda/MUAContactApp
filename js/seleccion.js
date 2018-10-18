eafcc1cb6cd159d4224c8a610a18e50307e7b61a
luri <luri@mujeresunidas.net>
2018-10-18 12:50:36 -0700

    Ya se limita solamente a 3 selecciones

--- js/seleccion.js
+++ js/seleccion.js
@@ -11,7 +11,33 @@ let checkboxes4 = document.querySelectorAll('fondos>input[type=checkbox]');
 /*Crear un event handler para darle clicks a los checkboxes, 
 Create an event handler for clicking checkbox groups*/
 
-object.onclick = function() {
+$('.input-radio100').on('change', function() {
+	if($('.input-radio100:checked').length > 3) {
+		this.checked = false;
+	}
+});
+
+
+
+/*function onlyOneOption(){
+	let checkboxselected= document.getElementById('checkboxselected').getElementByTagName("input");
+	let limit= 1;
+	for (let i=0; i < checkboxselected.length; i++) {
+		checkboxselected[i].onclick = function() {
+			let checkedcount =0;
+			for (let i=0; i < checkboxselected.length; i++) {
+				checkedcount += (checkboxselected[i].checked)? 1 : 0;
+			}
+			if (checkedcount > limit) {
+				alert("Solo puede seleccionar " + limit + " opciones");
+				this.checked = false;
+			}
+		}
+	}
+}
+
+
+/*object.onclick = function() {
 
 };
 
